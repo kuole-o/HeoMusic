8064e1aeae10704c3f13f97b00308a1379085444
GuoLe <guole.fun@qq.com>
2023-05-23 00:48:25 +0800

    🏖️ Music更新于 2023年05月23日  0:48:25

--- js/main.js
+++ js/main.js
@@ -8,61 +8,71 @@ const params = new URLSearchParams(window.location.search);
 var heo = {
   // 音乐节目切换背景
   changeMusicBg: function (isChangeBg = true) {
-    const heoMusicBg = document.getElementById("music_bg")
+    const heoMusicBg = document.getElementById("music_bg");
 
     if (isChangeBg) {
       // player loadeddata 会进入此处
       const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
-      var img = new Image();
+      const img = new Image();
       img.src = extractValue(musiccover.style.backgroundImage);
       img.onload = function() {
         heoMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
         
-        // 设置背景
-        var coverColor = colorfulImg(img.src);
-        console.log('本次提取到的歌曲封面主题色为：' + coverColor);
-        let body = document.body;
-        body.style.background = coverColor;
+        // 转发获取图片资源，解决跨域问题
+        const imgUrl = img.src;  // 图片 URL
+        const proxyUrl = "http://img-proxy.guole.fun/image-proxy?url=";  // 代理服务器的 URL
+
+        getDominantColor(proxyUrl + encodeURIComponent(imgUrl))
+          .then((color) => {
+            console.log("当前提取的主题色为:", color);
+            heo.setBodyBackgroundColor(color);
+          })
+          .catch((error) => {
+            console.error("错误:", error);
+          });
       };
     } else {
       // 第一次进入，绑定事件，改背景
       let timer = setInterval(()=>{
         const musiccover = document.querySelector("#heoMusic-page .aplayer-pic");
         // 确保player加载完成
-        // console.info(heoMusicBg);
         if (musiccover) {
-          clearInterval(timer)
+          clearInterval(timer);
           //初始化音量
           document.querySelector('meting-js').aplayer.volume(0.8,true);
           // 绑定事件
           heo.addEventListenerChangeMusicBg();
         }
-      }, 100)
+      }, 100);
     }
   },
   addEventListenerChangeMusicBg: function () {
     const heoMusicPage = document.getElementById("heoMusic-page");
     heoMusicPage.querySelector("meting-js").aplayer.on('loadeddata', function () {
       heo.changeMusicBg();
-      // console.info('player loadeddata');
     });
+  },
+  setBodyBackgroundColor: function (color) {
+    console.log('本次提取到的歌曲封面主题色为：' + color);
+    let body = document.body;
+    body.style.background = color;
   },
   getCustomPlayList: function() {
     const heoMusicPage = document.getElementById("heoMusic-page");
     const playlistType = params.get("type") || "playlist";
     
     if (params.get("id") && params.get("server")) {
-      console.log("获取到自定义内容")
-      var id = params.get("id")
-      var server = params.get("server")
+      console.log("获取到自定义内容");
+      var id = params.get("id");
+      var server = params.get("server");
       heoMusicPage.innerHTML = `<meting-js id="${id}" server="${server}" type="${playlistType}" mutex="true" preload="auto" order="random"></meting-js>`;
     } else {
-      console.log("无自定义内容")
+      console.log("无自定义内容");
       heoMusicPage.innerHTML = `<meting-js id="${userId}" server="${userServer}" type="${userType}" mutex="true" preload="auto" order="random"></meting-js>`;
     }
     heo.changeMusicBg(false);
   }
-}
+};
 
 // 调用
 heo.getCustomPlayList();
@@ -85,39 +95,59 @@ function extractValue(input) {
 }
 
 //歌曲封面获取主题色
-function colorfulImg(imgSrc) {
-  let img = new Image();
+function getDominantColor(imageSrc) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.src = imageSrc;
 
-  img.onload = function() {
-    let canvas = document.createElement('canvas'),
-        context = canvas.getContext && canvas.getContext('2d'),
-        height, width, length, data,
-        i = -4,
-        blockSize = 5,
-        count = 0,
-        rgb = {r:0, g:0, b:0};
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      canvas.width = img.width;
+      canvas.height = img.height;
 
-    height = canvas.height = img.height;
-    width = canvas.width = img.width;
-    context.drawImage(img, 0, 0);
-    data = context.getImageData(0, 0, width, height).data;
-    length = data.length;
+      // 绘制图像到画布
+      context.drawImage(img, 0, 0);
 
-    while ((i += blockSize * 4) < length) {
-      ++count;
-      rgb.r += data[i];
-      rgb.g += data[i + 1];
-      rgb.b += data[i + 2];
-    }
+      // 获取图像像素数据
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height).data;
 
-    rgb.r = ~~(rgb.r / count);
-    rgb.g = ~~(rgb.g / count);
-    rgb.b = ~~(rgb.b / count);
-    var color = 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
-    return color;
-  };
+      // 统计像素颜色值
+      const colorMap = {};
+      let maxCount = 0;
+      let dominantColor = null;
 
-  img.src = imgSrc;
+      for (let i = 0; i < imageData.length; i += 4) {
+        const r = imageData[i];
+        const g = imageData[i + 1];
+        const b = imageData[i + 2];
+        const rgb = `${r},${g},${b}`;
+
+        if (!colorMap[rgb]) {
+          colorMap[rgb] = 0;
+        }
+
+        colorMap[rgb] += 1;
+
+        if (colorMap[rgb] > maxCount) {
+          maxCount = colorMap[rgb];
+          dominantColor = rgb;
+        }
+      }
+
+      if (dominantColor) {
+        const color = `rgb(${dominantColor})`;
+        resolve(color);
+      } else {
+        reject(new Error("无法计算主题色。"));
+      }
+    };
+
+    img.onerror = function () {
+      reject(new Error("加载图像失败。"));
+    };
+  });
 }
 
 //空格控制音乐
